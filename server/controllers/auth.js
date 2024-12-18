c70317c1a454dd45b2e721e74ecb6b8854b521df
esraelif <elifesratunca@gmail.com>
2024-12-18 13:04:30 +0100

    setupProject

---
+++ server/controllers/auth.js
@@ -0,0 +1,18 @@
+
+const register = async (require, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+const login = async (require, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+module.exports = { register, login }
