b449c9f3533a70d512aff047fab6a73540a26ab0
esraelif <elifesratunca@gmail.com>
2024-12-23 22:49:21 +0100

    `feat: updated cSpell words, client subproject commit hash, and server auth controller`

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -30,17 +30,41 @@ const register = async (require, res) => {
         })
 
     } catch (error) {
-
+        return res.status(500).json({ msg: "error.message" })
     }
 }
 
+
+
+
 const login = async (require, res) => {
     try {
+        const { email, password } = req.body
+        const user = await AuthSchema.findOne(email)
+        if (!user) {
+            return res.status(500).json({ msg: 'There is not such a user!' })
+        }
+        const passwordCompare = await bcrypt.compare(password, user.password)
+        if (!passwordCompare) {
+            return res.status(500).json({ msg: 'Invalid password' })
+        }
+        const token = jwt.sign({ id: user._id }, "SECRET_KEY", { expiresIn: '1h' })
+        res.status(200).json({
+            status: 'OK',
+            user,
+            token
+        })
+
 
     } catch (error) {
         return res.status(500).json({ msg: "error.message" })
     }
 }
+
+
+
+
+
 function isEmail(emailAddress) {
     let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (emailAddress.match(regex))
